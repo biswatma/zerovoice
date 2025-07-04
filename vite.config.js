3520f525ce93e68a0f591031c1370ea985709023
biswatma <biswatma.nayak@gmail.com>
2025-07-04 12:43:24 +0530

    chore: Configure local development environment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/zerovoice/",
+  // base: "/zerovoice/",
   plugins: [tailwindcss(), react()],
   build: {
     target: "esnext",
